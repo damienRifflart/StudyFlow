c910956c3f09fc88417082d9b19fbf3055f6a7ef
damienRifflart <rifflartdamien68@gmail.com>
2025-08-02 15:15:38 +0200

    Can create an account wih quizzlify.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,6 +14,7 @@ use write_file::write_file;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             read_tree,
             read_file,
--- src/app/pages/Quiz.tsx
+++ src/app/pages/Quiz.tsx
@@ -1,10 +1,13 @@
 import { invoke } from "@tauri-apps/api/core";
-import { TriangleAlert } from "lucide-react";
+import { Info, TriangleAlert } from "lucide-react";
 import { useState } from "react";
+import { openUrl } from '@tauri-apps/plugin-opener'
 
 export default function Quiz() {
     const [loading, setLoading] = useState(false);
     const [quizLink, setQuizLink] = useState<string | null>(null);
+    const [isAuthenticated, setIsAuthenticated] = useState<string | null>(null);
+    const [userId, setUserId] = useState<string | null>(null);
     const [noteContent, setNoteContent] = useState<string | null>(null);
     const [filePath, setFilePath] = useState<string | null>(null)
 
@@ -84,13 +87,12 @@ Génère un objet JSON au format suivant (aucun texte en dehors du JSON) :
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
-                    author: "StudyFlow",
+                    author: isAuthenticated ? userId : "StudyFlow",
                     title: parsedQuiz.title,
                     created_at: new Date(),
                     content: parsedQuiz.content,
                     category: parsedQuiz.category,
                     level: parsedQuiz.level,
-                    status: "published"
                 }),
             });
 
@@ -129,64 +131,88 @@ Génère un objet JSON au format suivant (aucun texte en dehors du JSON) :
     return (
         <div className="w-full px-6 mt-8">
             <h1 className="text-3xl font-bold text-foreground mb-4">Quiz</h1>
-            <p className="text-lg mb-8 max-w-2xl">
-                Transformez vos notes en quiz interactifs et testez vos connaissances.
+            <p className="text-lg mb-8 w-full">
+                Transformez vos notes en quiz interactifs et testez vos connaissances. <br />
                 Grâce à l'API de <a href="https://quizzlify.vercel.app" target="_blank" className="text-accent hover:underline">Quizzlify</a>{" "}
                 et à celle d'OpenAI, profitez d'une génération intelligente de questions pour apprendre plus rapidement.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl">
+            <div className="grid grid-cols-3 gap-6">
                 {/* Étape 1 */}
-                <div className="p-6 bg-card rounded-2xl shadow-md border border-border">
-                    <h3 className="text-xl font-semibold mb-3">1. Choisissez une note</h3>
-                    <p className="mb-4">Sélectionnez une de vos notes pour générer un quiz adapté.</p>
+                <div className="p-6 bg-background2 rounded-2xl shadow-md border border-border flex flex-col justify-between h-full">
+                    <div>
+                        <h3 className="text-xl font-semibold mb-3">1. Choisissez une note</h3>
+                        <p className="mb-10">Sélectionnez une de vos notes pour générer un quiz adapté.</p>
+                    </div>
+
+                    {filePath && (
+                        <div>
+                            <p className="text-md text-blue-300">Fichier sélectionné: </p>
+                            <p
+                                className="overflow-hidden font-medium text-ellipsis"
+                                style={{
+                                    direction: "rtl",
+                                    textAlign: "left",
+                                    whiteSpace: "nowrap"
+                                }}
+                            >
+                                {filePath}
+                            </p>
+                        </div>
+                    )}
+
                     <button
                         onClick={handleSelectFile}
                         className="w-full py-3 px-4 rounded-xl bg-secondary font-medium"
                     >
                         Choisir une note
                     </button>
-
-                    {filePath && (
-                        <p
-                            className="mt-4 overflow-hidden font-medium text-ellipsis"
-                            style={{
-                                direction: "rtl",
-                                textAlign: "left",
-                                whiteSpace: "nowrap"
-                            }}
-                        >
-                            {filePath}
-                        </p>
-                    )}
                 </div>
 
                 {/* Étape 2 */}
-                <div className="p-6 bg-card rounded-2xl shadow-md border border-border">
-                    <h3 className="text-xl font-semibold mb-3">2. Créez votre quiz</h3>
-                    <p className="mb-2">Générez un quiz intelligent basé sur votre note.</p>
-                    <p className="mb-4 flex items-center text-orange-500">
-                        <TriangleAlert size={37} className="mr-2" />
-                            L’approbation de votre quiz par un administrateur de quizzlify peut prendre jusqu’à 7 jours.
+                <div className="p-6 bg-background2 rounded-2xl shadow-md border border-border flex flex-col justify-between h-full">
+                    <h3 className="text-xl font-semibold mb-3">2. Créer votre compte sur Quizzlify <span className="text-md font-normal">(facultatif)</span></h3>
+                    <p className="mb-2">Créez un compte gratuitement pour sauvegarder et rejouer vos quiz plus tard.</p>
+                    <p className="mb-4 flex items-center text-blue-300">
+                        <Info size={37} className="mr-2" />
+                        Étape facultative : créer un compte permet de sauvegarder, retrouver et éditer vos quiz.
                     </p>
 
                     <button
-                        onClick={handleGenerateQuiz}
-                        disabled={loading || !noteContent}
-                        className={"w-full py-3 px-4 rounded-xl font-medium bg-secondary"}
+                        onClick={() => openUrl("https://quizzlify.vercel.app/user/signup")}
+                        disabled={!noteContent}
+                        className={`w-full py-3 px-4 rounded-xl font-medium bg-secondary ${!noteContent ? "opacity-50 cursor-not-allowed" : ""}`}
                     >
-                        {loading ? "Génération..." : "Créer un Quiz"}
+                        {loading ? "Création du compte..." : "Créer un compte"}
                     </button>
+                </div>
+
+                {/* Étape 3 */}
+                <div className="p-6 bg-background2 rounded-2xl shadow-md border border-border flex flex-col justify-between h-full">
+                    <h3 className="text-xl font-semibold mb-3">3. Créez votre quiz</h3>
+                    <p className="mb-2">Générez un quiz intelligent basé sur votre note.</p>
+                    <p className="mb-4 flex items-center text-orange-500">
+                        <TriangleAlert size={37} className="mr-2" />
+                        L’approbation de votre quiz par un administrateur de Quizzlify peut prendre jusqu’à 7 jours.
+                    </p>
 
                     {quizLink && (
                         <p className="mt-4 text-green-600 font-medium">
                             ✅ Quiz créé !{" "}
 
-                            <a href={quizLink} className="hover:underline">
+                            <button onClick={() => openUrl(quizLink)} className="hover:underline">
                                 Voir le quiz
-                            </a>
+                            </button>
                         </p>
                     )}
+
+                    <button
+                        onClick={handleGenerateQuiz}
+                        disabled={loading || !noteContent}
+                        className={`w-full py-3 px-4 rounded-xl font-medium bg-secondary ${loading || !noteContent ? "opacity-50 cursor-not-allowed" : ""}`}
+                    >
+                        {loading ? "Génération..." : "Créer un Quiz"}
+                    </button>
                 </div>
             </div>
         </div>
